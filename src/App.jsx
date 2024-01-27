04d51b6914ce0779118fcd790e5a26556d3f404b
Hunter Termo <huntertermo@yahoo.com>
2024-01-26 22:58:50 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>All the Tech News</h1>
+      <h3>Here are all the best sites to get your Tech News from.</h3>
+      <Grid />
+    </div>
+  )
+}
+
+export default App;
